6d769e6fe6eeb35b359275ddfca2b45a91600295
fuadmiazi <fuad.hasan.1997@gmail.com>
2024-05-31 10:55:22 +0600

    Meta tag added

--- src/components/Reference/Reference.jsx
+++ src/components/Reference/Reference.jsx
@@ -5,7 +5,7 @@ const Reference = () => {
   return (
     <div className='reference'>
       <h1>Reference</h1>
-      <h4>MAJOR. DR. MD. TOWFIQURRAHMAN (RETD.)</h4>
+      <h4>MAJOR. DR. MD. TOWFIQUR RAHMAN (RETD.)</h4>
       <p>CEO at Banglafire Solution Ltd.</p>
       <p>Email: md@banglafire.com</p>
       <p>Phone: +880 1711019518</p>
